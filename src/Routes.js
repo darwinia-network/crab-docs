a2978abeb8b10d370a085a4f0cf627ab1b4e2a11
JayJay1024 <lygm1062@gmail.com>
2021-03-17 14:53:56 +0800

    Loading

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,26 @@
+import { Suspense, lazy } from 'react';
+import { Spin } from 'antd';
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
-import { asyncComponent } from "./components/AsyncComponent";
+// import { asyncComponent } from "./components/AsyncComponent";
 
 const history = createBrowserHistory();
 
-const AsyncHome = asyncComponent(import("./page/Home"));
-const AsyncNotFound = asyncComponent(import("./page/NotFound"));
+// const AsyncHome = asyncComponent(import("./page/Home"));
+// const AsyncNotFound = asyncComponent(import("./page/NotFound"));
 
 const Routers = () => (
     <Router history={history}>
-        <Switch>
-            <Route
-                exact
-                component={AsyncHome}
-                path='/'
-            />
-            <Route component={AsyncNotFound} />
-        </Switch>
+        <Suspense fallback={<Spin size='large' style={{ marginTop: '40%' }} />}>
+            <Switch>
+                <Route
+                    exact
+                    component={lazy(() => import('./page/Home'))}
+                    path='/'
+                />
+                <Route component={lazy(() => import('./page/NotFound'))} />
+            </Switch>
+        </Suspense>
     </Router>
 );
 
