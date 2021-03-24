42059a6fa5afb4f3a6cd524dd9523fc2f569e656
Aki Wu <wuminzhe@gmail.com>
2021-03-24 16:10:19 +0800

    Update params

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-			'https://github.com/darwinia-network/crab-home/edit/master/docs/',
+			'https://github.com/darwinia-network/crab-home/edit/master/',
         },
         blog: {
           showReadingTime: true,
