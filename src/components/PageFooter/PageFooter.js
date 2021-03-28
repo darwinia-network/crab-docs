3a193f6752da6b796cb94a28b27b1d4504ab23b9
JayJay1024 <lygm1062@gmail.com>
2021-03-28 18:20:41 +0800

    Fixed footer bg

--- src/components/PageFooter/PageFooter.js
+++ src/components/PageFooter/PageFooter.js
@@ -3,7 +3,7 @@ import styles from './style.module.scss';
 
 const PageFooter = () => {
     return (
-        <div className={styles.container}>
+        <div className={styles.crabContainer}>
             <div className={styles.content}>
                 <span>Copyright©2021 Crab.Network</span>
             </div>
