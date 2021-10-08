017e39c787ad7085ddaedfca969faf88d21f66aa
Jay <lygm1062@gmail.com>
2021-10-08 15:41:10 +0800

    fix: edit url (#51)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,15 +92,8 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/darwinia-network/crab-docs/',
+          editUrl: 'https://github.com/darwinia-network/crab-docs/edit/main/',
           routeBasePath: '/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.scss'),
