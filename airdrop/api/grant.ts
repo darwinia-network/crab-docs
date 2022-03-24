73957b738419d0ab62d46109b3cdbbb1927797f5
JayJay1024 <lygm1062@gmail.com>
2022-03-24 22:37:14 +0800

    fix: github oauth between crab and pangolin

--- airdrop/api/grant.ts
+++ airdrop/api/grant.ts
@@ -2,18 +2,15 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 const grant = require('grant').vercel({
   config: grantConfig(), session: {secret: 'grant'}
-})
-
-
+});
 
 function grantConfig() {
   const config = require('./config/grant.json');
-  config.github.key = process.env.PANGOLIN_GITHUB_OAUTH_APP_KEY;
-  config.github.secret = process.env.PANGOLIN_GITHUB_OAUTH_APP_SECRET;
+  config.github.key = process.env.GITHUB_OAUTH_APP_KEY;
+  config.github.secret = process.env.GITHUB_OAUTH_APP_SECRET;
   return config;
 }
 
 export default async function (req: VercelRequest, res: VercelResponse) {
-  
   await grant(req, res)
 }
