e51f70a9a8ffc829b5a07a801523a89b4e5c2804
JayJay1024 <lygm1062@gmail.com>
2022-03-24 22:46:25 +0800

    fix: build fail

--- airdrop/api/grant.ts
+++ airdrop/api/grant.ts
@@ -1,15 +1,36 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-const grant = require('grant').vercel({
-  config: grantConfig(), session: {secret: 'grant'}
-});
-
 function grantConfig() {
-  const config = require('./config/grant.json');
+  const config = {
+    "defaults": {
+      // "origin": "https://docs.crab.network",
+      "origin": "https://crab-docs-dev.vercel.app",
+      "transport": "querystring",
+      "state": true
+    },
+    "github": {
+      "key": "{{ github_oauth_app_key }}",
+      "secret": "{{ github_oauth_app_secret }}",
+      "scope": [],
+      "callback": "/api/crab/authorization",
+      "overrides": {
+        "crab": {
+          "callback": "/api/crab/authorization"
+        },
+        "pangolin": {
+          "callback": "/api/pangolin/authorization"
+        }
+      }
+    }
+  };
   config.github.key = process.env.GITHUB_OAUTH_APP_KEY;
   config.github.secret = process.env.GITHUB_OAUTH_APP_SECRET;
   return config;
 }
+
+const grant = require('grant').vercel({
+  config: grantConfig(), session: {secret: 'grant'}
+});
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   await grant(req, res)
