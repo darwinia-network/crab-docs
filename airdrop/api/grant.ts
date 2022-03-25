31a66f432bfe8169d620395ac6bc404b182fc7e3
cinlk <wayne.liang@itering.io>
2022-03-25 10:06:25 +0800

    chore: grant configration

--- airdrop/api/grant.ts
+++ airdrop/api/grant.ts
@@ -1,17 +1,35 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-function grantConfig() {
-  const config = require("./grantConfig.json");
+function pangolinGrantConfig() {
+  const config = require("./pangolin/config/grant.json");
   config.github.key = process.env.PANGOLIN_GITHUB_OAUTH_APP_KEY;
   config.github.secret = process.env.PANGOLIN_GITHUB_OAUTH_APP_SECRET;
   return config;
 }
 
-const grant = require("grant").vercel({
-  config: grantConfig(),
-  session: { secret: "grant" },
+const pangolinGrant = require("grant").vercel({
+  config: pangolinGrantConfig(),
+  session: { secret: "pangolin-grant" },
 });
 
+const crabGrant = require('grant').vercel({
+  config: crabGrantConfig(), session: {secret: 'crab-grant'}
+})
+
+
+function crabGrantConfig() {
+  const config = require('./crab/config/grant.json');
+  config.github.key = process.env.GITHUB_OAUTH_APP_KEY;
+  config.github.secret = process.env.GITHUB_OAUTH_APP_SECRET;
+  return config;
+}
+
 export default async function (req: VercelRequest, res: VercelResponse) {
-  await grant(req, res);
+  console.log(`request url ${req.url}`);
+  if ("/connect/github/pangolin" === req.url){
+    await pangolinGrant(req, res);
+  }else{
+    await crabGrant(req, res);
+  }
+  
 }
