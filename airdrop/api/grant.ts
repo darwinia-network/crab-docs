2326b0bf93a74b9d2262c5eb9235afb5e4696be1
JayJay1024 <lygm1062@gmail.com>
2022-03-25 08:47:22 +0800

    chore: update grant config

--- airdrop/api/grant.ts
+++ airdrop/api/grant.ts
@@ -1,34 +1,9 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 function grantConfig() {
-  const config = {
-    defaults: {
-      // "origin": "https://docs.crab.network",
-      origin: "https://crab-docs-dev.vercel.app",
-      transport: "querystring",
-      state: true,
-    },
-    github: {
-      key: "{{ github_oauth_app_key }}",
-      secret: "{{ github_oauth_app_secret }}",
-      scope: [],
-      callback: "/api/crab/authorization",
-      overrides: {
-        crab: {
-          // key: process.env.GITHUB_OAUTH_APP_KEY,
-          // secret: process.env.GITHUB_OAUTH_APP_SECRET,
-          key: process.env.PANGOLIN_GITHUB_OAUTH_APP_KEY,
-          secret: process.env.PANGOLIN_GITHUB_OAUTH_APP_SECRET,
-          callback: "/api/crab/authorization",
-        },
-        pangolin: {
-          key: process.env.PANGOLIN_GITHUB_OAUTH_APP_KEY,
-          secret: process.env.PANGOLIN_GITHUB_OAUTH_APP_SECRET,
-          callback: "/api/pangolin/authorization",
-        },
-      },
-    },
-  };
+  const config = require("./grantConfig.json");
+  config.github.key = process.env.PANGOLIN_GITHUB_OAUTH_APP_KEY;
+  config.github.secret = process.env.PANGOLIN_GITHUB_OAUTH_APP_SECRET;
   return config;
 }
 
