a961dad2334e477675e3255560a01bdfac7c27ac
JayJay1024 <lygm1062@gmail.com>
2022-03-25 12:08:03 +0800

    chore: update amount for testing

--- airdrop/api/pangolin/airdrop/transfer.ts
+++ airdrop/api/pangolin/airdrop/transfer.ts
@@ -10,7 +10,7 @@ import is from "is_js";
 
 const Redis = require("ioredis");
 
-const AMOUNT = 100;
+const AMOUNT = 10;
 
 // request
 export default async function (req: VercelRequest, res: VercelResponse) {
